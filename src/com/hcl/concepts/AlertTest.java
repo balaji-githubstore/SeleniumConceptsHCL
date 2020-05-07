ae9bd754dc5152b6780e01341574bb9facb5a4ce
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2020-05-07 18:06:02 +0530

    third commit

--- src/com/hcl/concepts/AlertTest.java
+++ src/com/hcl/concepts/AlertTest.java
@@ -7,12 +7,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class AlertTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new InternetExplorerDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
